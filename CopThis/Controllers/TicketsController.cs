32d521286f81c5e9eacc0e10832eca7dbbf400c8
XwipeoutX <xwipeoutx@gmail.com>
2016-02-23 22:06:05 +1000

    Initial Commit

---
+++ CopThis/Controllers/TicketsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CopThis.Controllers
+{
+    public class TicketsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CopThis/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CopThis.MvcApplication" Language="C#" %>
